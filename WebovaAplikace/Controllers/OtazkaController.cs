9c2cd5cdc55d9072d689470a17157a421c70d48c
Alfred <kufaafred@gmail.com>
2022-11-08 18:36:16 +0100

    Zmena na MVC design + zaklad

---
+++ WebovaAplikace/Controllers/OtazkaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebovaAplikace.Models;
+
+
+namespace WebovaAplikace.Controllers
+{
+    public class OtazkaController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogviewModel doggo = new DogviewModel() { Name = "Sif", age = 2 };
+            return View(doggo);
+        }
+
+        public string Otazka()
+        {
+            return "Zadejte hodnotu penezenky";
+        }
+
+    }
+}
---
+++ WebovaAplikace/Models/DogviewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebovaAplikace.Models
+{
+    public class DogviewModel
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
